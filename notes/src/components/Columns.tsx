ea66fb2d9aaa20945bcb4623ac6c6d92be3a4da8
Boris Karakolev <boriskarakolev@hotmail.com>
2022-06-02 15:55:39 +0200

    Fix small things

--- notes/src/components/Columns.tsx
+++ notes/src/components/Columns.tsx
@@ -5,27 +5,22 @@ export const columns = [
       {
         Header: "id",
         accessor: "id",
-        getSortByToggleProps: () => null
       },
       {
         Header: "Name",
         accessor: "name",
-        getSortByToggleProps: () => null
       },
       {
         Header: "Date",
         accessor: "date",
-        getSortByToggleProps: () => null
       },
       {
         Header: "Content",
         accessor: "content",
-        getSortByToggleProps: () => null
       },
       {
         Header: "Edit",
         accessor: "Edit",
-        getSortByToggleProps: () => null
       },
     ],
   },
